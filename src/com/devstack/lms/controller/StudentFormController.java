6ca1746f967bdab847f63802a14fa37879d17df8
ozen-hub <info.ozenhub@gmail.com>
2024-05-30 21:41:53 +0530

    1

--- src/com/devstack/lms/controller/StudentFormController.java
+++ src/com/devstack/lms/controller/StudentFormController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 public class StudentFormController {
@@ -18,17 +19,32 @@ public class StudentFormController {
 
     public void saveStudentOnAction(ActionEvent actionEvent) {
 
-        Student student= new Student(
-                UUID.randomUUID().toString(),
-                txtName.getText().trim(),
-                txtAddress.getText().trim(),
-                txtEmail.getText().toLowerCase().trim(),
-                Integer.parseInt(txtAge.getText())
-        );
-        DatabaseAccessCode databaseAccessCode = new DatabaseAccessCode();
-        boolean isSaved = databaseAccessCode.saveStudent(student);
-        if (isSaved){
-            new Alert(Alert.AlertType.INFORMATION,"Student has been saved..", ButtonType.CLOSE).show();
+        try {
+            Student student = new Student(
+                    UUID.randomUUID().toString(),
+                    txtName.getText().trim(),
+                    txtAddress.getText().trim(),
+                    txtEmail.getText().toLowerCase().trim(),
+                    Integer.parseInt(txtAge.getText())
+            );
+            DatabaseAccessCode databaseAccessCode = new DatabaseAccessCode();
+            boolean isSaved = databaseAccessCode.saveStudent(student);
+            if (isSaved) {
+                new Alert(Alert.AlertType.INFORMATION, "Student has been saved..", ButtonType.CLOSE).show();
+                clearFields();
+            } else {
+                new Alert(Alert.AlertType.WARNING, "Try Again..", ButtonType.CLOSE).show();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.CLOSE).show();
         }
     }
+
+    private void clearFields(){
+        txtName.clear();
+        txtEmail.clear();
+        txtAddress.clear();
+        txtAge.clear();
+    }
+
 }
--- src/com/devstack/lms/db/DatabaseAccessCode.java
+++ src/com/devstack/lms/db/DatabaseAccessCode.java
@@ -2,10 +2,31 @@ package com.devstack.lms.db;
 
 import com.devstack.lms.model.Student;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DatabaseAccessCode {
 
-    public boolean saveStudent(Student student){
-        // save code
+    public boolean saveStudent(Student student) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/devstack_lms", "root", "1234");
+
+        String sql="INSERT INTO student VALUES(?,?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,student.getStudentId());
+        preparedStatement.setString(2,student.getStudentName());
+        preparedStatement.setString(3,student.getAddress());
+        preparedStatement.setInt(4,student.getAge());
+        preparedStatement.setString(5,student.getEmail());
+
+        int affectedRowCount = preparedStatement.executeUpdate();
+
+        if(affectedRowCount>0){
+          return true;
+        }
         return false;
     }
 }
