1efe73ec1292dc0041ef53de3b3819a4d491b966
ozen-hub <info.ozenhub@gmail.com>
2024-06-03 20:38:20 +0530

    12

--- src/com/devstack/lms/controller/CourseFormController.java
+++ src/com/devstack/lms/controller/CourseFormController.java
@@ -1,4 +1,29 @@
 package com.devstack.lms.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class CourseFormController {
+    public AnchorPane context;
+    public TextField txtCourseName;
+    public Button btnSave;
+    public TextField txtCourseFee;
+    public TextField txtSearch;
+    public TableView tblCourses;
+    public TableColumn colCourseName;
+    public TableColumn colCourseFee;
+    public TableColumn colOption;
+
+    public void saveCourseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewCourseOnAction(ActionEvent actionEvent) {
+    }
 }
