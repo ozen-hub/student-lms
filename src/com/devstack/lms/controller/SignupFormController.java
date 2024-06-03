aa366334de1d9aefb5f641f637a6f7a255931811
ozen-hub <info.ozenhub@gmail.com>
2024-06-03 19:41:28 +0530

    7

--- src/com/devstack/lms/controller/SignupFormController.java
+++ src/com/devstack/lms/controller/SignupFormController.java
@@ -1,8 +1,11 @@
 package com.devstack.lms.controller;
 
+import com.devstack.lms.db.DatabaseAccessCode;
+import com.devstack.lms.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -10,6 +13,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.UUID;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -17,6 +22,29 @@ public class SignupFormController {
     public PasswordField txtPassword;
 
     public void createNewAccountOnAction(ActionEvent actionEvent) {
+        User user = new User(
+                UUID.randomUUID().toString(),
+                txtUsername.getText(),
+                txtPassword.getText()
+        );
+        DatabaseAccessCode databaseAccessCode = new DatabaseAccessCode();
+        try {
+            boolean isSaved = databaseAccessCode.signup(user);
+            if (isSaved) {
+                new Alert(Alert.AlertType.INFORMATION, "Sign up process completed").show();
+                URL resource = getClass().getResource("../view/LoginForm.fxml");
+                Stage stage = (Stage) context.getScene().getWindow();
+                stage.centerOnScreen();
+                stage.setScene(new Scene(FXMLLoader.load(resource)));
+                stage.setTitle("Login Form");
+            } else {
+                new Alert(Alert.AlertType.WARNING, "something went wrong, pleas try again a bit later..").show();
+            }
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
     }
 
     public void navigateToLoginFormOnAction(ActionEvent actionEvent) throws IOException {
--- src/com/devstack/lms/db/DatabaseAccessCode.java
+++ src/com/devstack/lms/db/DatabaseAccessCode.java
@@ -1,54 +1,55 @@
 package com.devstack.lms.db;
 
 import com.devstack.lms.model.Student;
+import com.devstack.lms.model.User;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseAccessCode {
-
+    //=====Student management==========
     public boolean saveStudent(Student student) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/devstack_lms", "root", "1234");
 
-        String sql="INSERT INTO student VALUES(?,?,?,?,?)";
+        String sql = "INSERT INTO student VALUES(?,?,?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,student.getStudentId());
-        preparedStatement.setString(2,student.getStudentName());
-        preparedStatement.setString(3,student.getAddress());
-        preparedStatement.setInt(4,student.getAge());
-        preparedStatement.setString(5,student.getEmail());
+        preparedStatement.setString(1, student.getStudentId());
+        preparedStatement.setString(2, student.getStudentName());
+        preparedStatement.setString(3, student.getAddress());
+        preparedStatement.setInt(4, student.getAge());
+        preparedStatement.setString(5, student.getEmail());
 
         int affectedRowCount = preparedStatement.executeUpdate();
 
-        if(affectedRowCount>0){
-          return true;
+        if (affectedRowCount > 0) {
+            return true;
         }
         return false;
     }
 
     public List<Student> findAllStudents(String searchText) throws ClassNotFoundException, SQLException {
-        searchText="%"+searchText+"%";
+        searchText = "%" + searchText + "%";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/devstack_lms", "root", "1234");
 
-        String sql="SELECT * FROM student WHERE student_name LIKE ? OR email LIKE ?";
+        String sql = "SELECT * FROM student WHERE student_name LIKE ? OR email LIKE ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,searchText);
-        preparedStatement.setString(2,searchText);
+        preparedStatement.setString(1, searchText);
+        preparedStatement.setString(2, searchText);
 
         ResultSet resultSet = preparedStatement.executeQuery();
 
         List<Student> studentList = new ArrayList<>();
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             studentList.add(new Student(
                     resultSet.getString(1), resultSet.getString(2),
-                    resultSet.getString(3),resultSet.getString(5),
+                    resultSet.getString(3), resultSet.getString(5),
                     resultSet.getInt(4)
             ));
         }
@@ -60,13 +61,13 @@ public class DatabaseAccessCode {
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/devstack_lms", "root", "1234");
 
-        String sql="DELETE FROM student WHERE student_id=?";
+        String sql = "DELETE FROM student WHERE student_id=?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,id);
+        preparedStatement.setString(1, id);
 
         int affectedRowCount = preparedStatement.executeUpdate();
 
-        if(affectedRowCount>0){
+        if (affectedRowCount > 0) {
             return true;
         }
         return false;
@@ -77,17 +78,17 @@ public class DatabaseAccessCode {
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/devstack_lms", "root", "1234");
 
-        String sql="UPDATE student SET student_name=?, address=?, age=?,email=? WHERE student_id=?";
+        String sql = "UPDATE student SET student_name=?, address=?, age=?,email=? WHERE student_id=?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,student.getStudentName());
-        preparedStatement.setString(2,student.getAddress());
-        preparedStatement.setInt(3,student.getAge());
-        preparedStatement.setString(4,student.getEmail());
-        preparedStatement.setString(5,student.getStudentId());
+        preparedStatement.setString(1, student.getStudentName());
+        preparedStatement.setString(2, student.getAddress());
+        preparedStatement.setInt(3, student.getAge());
+        preparedStatement.setString(4, student.getEmail());
+        preparedStatement.setString(5, student.getStudentId());
 
         int affectedRowCount = preparedStatement.executeUpdate();
 
-        if(affectedRowCount>0){
+        if (affectedRowCount > 0) {
             return true;
         }
         return false;
@@ -98,20 +99,46 @@ public class DatabaseAccessCode {
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/devstack_lms", "root", "1234");
 
-        String sql="SELECT * FROM student WHERE student_id=?";
+        String sql = "SELECT * FROM student WHERE student_id=?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,studentId);
+        preparedStatement.setString(1, studentId);
 
         ResultSet resultSet = preparedStatement.executeQuery();
 
 
-        if (resultSet.next()){
+        if (resultSet.next()) {
             return new Student(
                     resultSet.getString(1), resultSet.getString(2),
-                    resultSet.getString(3),resultSet.getString(5),
+                    resultSet.getString(3), resultSet.getString(5),
                     resultSet.getInt(4)
             );
         }
         return null;
     }
+
+    //=====Student management==========
+
+    //=====User management==========
+    public boolean signup(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/devstack_lms",
+                        "root",
+                        "1234");
+
+        String sql = "INSERT INTO user_table VALUES(?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, user.getUserId());
+        preparedStatement.setString(2, user.getUsername());
+        preparedStatement.setString(3, user.getPassword());
+
+        int affectedRowCount = preparedStatement.executeUpdate();
+
+        if (affectedRowCount > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    //=====User management==========
 }
